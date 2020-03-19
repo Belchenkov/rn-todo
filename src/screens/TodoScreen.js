f061f29570cb2d5f3341dcdfda0ff5ab04827f2e
Aleksey Belchenkov <u608110@gmail.com>
2020-03-19 18:39:05 +0300

    Styles for Buttons

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -4,14 +4,41 @@ import { View, StyleSheet, Text, Button } from "react-native";
 const TodoScreen = ({ goBack, todo }) => {
     return (
         <View>
-            <Text>{ todo.title }</Text>
-            <Button title='Назад' onPress={goBack} />
+            <Text style={styles.title}>{ todo.title }</Text>
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <Button
+                        title='Назад'
+                        color='#757575'
+                        onPress={goBack}
+                    />
+                </View>
+                <View style={styles.button}>
+                    <Button
+                        title='Удалить'
+                        color='#e53935'
+                        onPress={goBack}
+                    />
+                </View>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20
+    },
+    button: {
+        width: '40%',
+        borderRadius: 10
+    },
+    title: {
+        textAlign: 'center',
+        fontWeight: 'bold'
+    }
 });
 
 export default TodoScreen;
