98713615a94a0b753e22f1fd65d62a8375847b3a
Aleksey Belchenkov <u608110@gmail.com>
2020-03-23 17:56:02 +0300

    Create Modal Window

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text, Button } from "react-native";
 
 import {THEME} from "../theme";
 import AppCard from "../components/ui/AppCard";
+import EditModal from "../components/EditModal";
 
 const TodoScreen = ({ goBack, todo, onRemove }) => {
+    const [modal, setModal] = useState(false);
+
     return (
         <View>
+            <EditModal visible={modal} onCancel={() => setModal(false)} />
             <AppCard style={styles.card}>
                 <Text style={styles.title}>{ todo.title }</Text>
-                <Button title='Редактировать' />
+                <Button
+                    title='Редактировать'
+                    onPress={() => setModal(true)}
+                />
             </AppCard>
             <View style={styles.buttons}>
                 <View style={styles.button}>
