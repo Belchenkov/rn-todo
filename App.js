04d5bfedf29aa092471f41d0720737a8581423e6
Aleksey Belchenkov <u608110@gmail.com>
2020-03-23 17:28:11 +0300

    Window Confirm

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 
 import Navbar from './src/components/Navbar';
 import MainScreen from "./src/screens/MainScreen";
@@ -20,7 +20,26 @@ export default function App() {
   };
 
   const removeTodo = id => {
-      setTodos(prev => prev.filter(todo => todo.id !== id));
+      const todo = todos.find(t => t.id === id);
+
+      Alert.alert(
+          'Удаление элемента',
+          `Вы действительно хотите удалить ${todo.title}?`,
+          [
+              {
+                  text: 'Отмена',
+                  style: 'cancel',
+              },
+              {
+                  text: 'Удалить',
+                  style: 'negative',
+                  onPress: () => {
+                      setTodoId();
+                      setTodos(prev => prev.filter(todo => todo.id !== id));
+                  }},
+          ],
+          {cancelable: false},
+      );
   };
 
   let content = (<MainScreen
@@ -34,6 +53,7 @@ export default function App() {
       const selectedTodo = todos.find(todo => todo.id === todoId);
 
       content = <TodoScreen
+          onRemove={removeTodo}
           goBack={() => setTodoId(null)}
           todo={selectedTodo}
       />
