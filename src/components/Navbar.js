040dc5554225f3287bc03450cbd2cf90e006a496
Aleksey Belchenkov <u608110@gmail.com>
2020-03-30 18:45:34 +0300

    Define Platform

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Platform } from 'react-native'
 
 import {THEME} from "../theme";
 import AppTextBold from "./ui/AppTextBold";
@@ -7,7 +7,10 @@ import AppTextBold from "./ui/AppTextBold";
 
 const Navbar = ({ title }) => {
     return (
-        <View style={styles.navbar}>
+        <View style={{...styles.navbar, ...Platform.select({
+                ios: styles.navbarIos,
+                android: styles.navbarAndroid
+            })}}>
             <AppTextBold style={styles.text}>{title}</AppTextBold>
         </View>
     )
@@ -20,11 +23,17 @@ const styles = StyleSheet.create({
         height: 70,
         alignItems: 'center',
         justifyContent: 'flex-end',
-        backgroundColor: THEME.MAIN_COLOR,
         paddingBottom: 10
     },
+    navbarAndroid: {
+        backgroundColor: THEME.MAIN_COLOR,
+    },
+    navbarIos: {
+        borderBottomColor: THEME.MAIN_COLOR,
+        borderBottomWidth: 1
+    },
     text: {
-        color: 'white',
+        color: Platform.OS === 'ios' ? THEME.MAIN_COLOR : 'white',
         fontWeight: 'bold',
         fontSize: 20
     }
