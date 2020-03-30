8455b6c4996ea92d1c35a9627d5946ebaf25d285
Aleksey Belchenkov <u608110@gmail.com>
2020-03-30 18:50:57 +0300

    Adaptive Styles

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, Dimensions } from "react-native";
 import { FontAwesome, AntDesign } from "@expo/vector-icons";
 
 import {THEME} from "../theme";
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
     },
     button: {
-        width: '40%',
+        width: Dimensions.get('window').width / 3,
         borderRadius: 10
     },
     title: {
